d59c370b5f01513fa67885b55806fad29c62cb43
Pierre de Wet <pdewet.bitc@gmail.com>
2016-08-24 20:43:37 +0100

    Rename files to something sensible
    renames media files

---
+++ renametv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+shopt -s nullglob
+for f in *.{mkv,mp4,m4v,avi}; do 
+	newname=`echo "$f" | sed -e 's/\(S[0-9][0-9]E[0-9][0-9]\).*\.\([am][kp4v][4vi]\)/\1.\2/'` 
+	mv "$f" "$newname"; 
+done
