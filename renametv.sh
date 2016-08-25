f5a41b4f2e0586c33e83d7a13d46ddc66dd0f7f3
Pierre de Wet <pdewet.bitc@gmail.com>
2016-08-25 11:00:38 +0100

    Update renametv.sh

--- renametv.sh
+++ renametv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s nullglob
 for f in *.{mkv,mp4,m4v,avi}; do 
-	newname=`echo "$f" | sed -e 's/\(S[0-9][0-9]E[0-9][0-9]\).*\.\([am][kp4v][4vi]\)/\1.\2/'` 
+	newname=`echo "$f" | sed -e 's/\(S[0-9][0-9]E[0-9][0-9]\).*\.\([am][4kpv][4iv]\)/\1.\2/'` 
 	mv "$f" "$newname"; 
 done
